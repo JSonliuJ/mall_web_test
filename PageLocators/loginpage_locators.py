891a078bdbb7c1559e3040514bb027d8b3fbb620
JSonliuJ <492224300@qq.com>
2022-10-12 03:25:19 +0800

    login_locator

---
+++ PageLocators/loginpage_locators.py
@@ -0,0 +1,20 @@
+# —— coding :utf-8 ——
+# @time:    2020/10/7 21:00
+# @IDE:     webTest_frmewok_v2.0
+# @Author:  jsonJie
+# @Email:   810030907@qq.com
+# @File:    loginpage_locators.py
+
+from selenium.webdriver.common.by import By
+class LoginPageLocator:
+    ''''元素定位类'''
+    # 输入用户名
+    username_input_locator = (By.XPATH, '//input[@placeholder="请输入手机号/用户名"]')
+    # 输入密码
+    password_input_locator = (By.XPATH, '//input[@placeholder="请输入密码"]')
+    # 确认登录
+    confirm_login_locator = (By.XPATH, '//a[@class="login-button"]')
+    # 错误信息
+    error_msg_locator = (By.XPATH, '//div[@class="error"]')
+    # 用户名信息
+    user_name = ('xpath', f'//a[text()="#username#"]')
