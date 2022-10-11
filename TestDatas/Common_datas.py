288e3173b1368d45c194bc7a8ebe6fa1d5339957
JSonliuJ <492224300@qq.com>
2022-10-12 03:43:34 +0800

    common_data

---
+++ TestDatas/Common_datas.py
@@ -0,0 +1,17 @@
+# —— coding :utf-8 ——
+# @time:    2020/10/7 21:02
+# @IDE:     webTest_frmewok_v2.0
+# @Author:  jsonJie
+# @Email:   810030907@qq.com
+# @File:    Common_datas.py
+# 全局 --系统访问地址 --登录链接
+class Config:
+    db = dict(
+        host = 'xxxx',
+        port = 3306,
+        user = 'xxx',
+        password = 'xxx',
+        database = 'xxx'
+    )
+
+    web_login_url = "http://xxxx"
