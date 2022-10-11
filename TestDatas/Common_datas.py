38071fcc6c72a2bdde649df0c7b9eddb5248e124
JSonliuJ <492224300@qq.com>
2022-10-12 03:53:50 +0800

    delete

--- TestDatas/Common_datas.py
+++ TestDatas/Common_datas.py
@@ -7,11 +7,11 @@
 # 全局 --系统访问地址 --登录链接
 class Config:
     db = dict(
-        host = 'xxxx',
+        host = '47.113.180.81',
         port = 3306,
-        user = 'xxx',
-        password = 'xxx',
-        database = 'xxx'
+        user = 'lemon',
+        password = 'lemon123',
+        database = 'yami_shops'
     )
 
-    web_login_url = "http://xxxx"
+    web_login_url = "http://mall.lemonban.com:3344/"
