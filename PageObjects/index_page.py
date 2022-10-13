4ef654de12877fec52ba68d9f6d64c050fee4eac
JSonliuJ <492224300@qq.com>
2022-10-13 16:14:24 +0800

    pageobject

--- PageObjects/index_page.py
+++ PageObjects/index_page.py
@@ -11,6 +11,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from PageLocators import indexpage_locators as loc
+
 class IndexPage:
     def __init__(self,driver):
         self.driver = driver
